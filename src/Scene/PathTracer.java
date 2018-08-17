912bdc6dced1d86e0fea75df58e83e9079d468c5
albaNoodle <albiclevi@gmail.com>
2018-08-17 12:59:36 +0200

    if refracta

--- src/Scene/PathTracer.java
+++ src/Scene/PathTracer.java
@@ -15,14 +15,13 @@ import Figures.*;
 
 public class PathTracer {
 
-	
 	final static int MAX_REBOTES = 3;
 	public static double profImagen = 10;
 	public static int antialiasing = 30;
 	public static List<Figure> figures = new ArrayList<Figure>();
 	private static Direction Nb;
 	private static Direction Nt;
-	private static int percent =0;
+	private static int percent = 0;
 
 	public static void main(String[] args) {
 		// Resolution photo
@@ -60,28 +59,28 @@ public class PathTracer {
 
 		// List of figures
 
-		figures.add(new Plane(new Point(0, 1, 0), new Direction(0, -1, 0), Color.MAGENTA, 40, 0.5, 0.0)); // arriba
-		figures.add(new Plane(new Point(0, -1, 0), new Direction(0, 1, 0), Color.MAGENTA, 40, 0.5, 0.0));// abajo
-		figures.add(new Plane(new Point(0, 0, 1), new Direction(0, 0, -1), Color.WHITE, 90, 0.5,0 )); // fondo
-		figures.add(new Plane(new Point(0, 0, 1), new Direction(0, 0, 1), Color.YELLOW, 40, 0.5, 0)); //
-		figures.add(new Plane(new Point(1, 0, 0), new Direction(-1, 0, 0), Color.RED, 0, 0.5, 0)); // izq
-		figures.add(new Plane(new Point(-1, 0, 0), new Direction(1, 0, 0), Color.GREEN, 80, 0.5, 0));// drcha
+		figures.add(new Plane(new Point(0, 1, 0), new Direction(0, -1, 0), Color.MAGENTA, 40, 0.5, 0.0, 0.0)); // arriba
+		figures.add(new Plane(new Point(0, -1, 0), new Direction(0, 1, 0), Color.MAGENTA, 40, 0.5, 0.0, 0.0));// abajo
+		figures.add(new Plane(new Point(0, 0, 1), new Direction(0, 0, -1), Color.WHITE, 90, 0.5, 0, 0.0)); // fondo
+		figures.add(new Plane(new Point(0, 0, 1), new Direction(0, 0, 1), Color.YELLOW, 40, 0.5, 0, 0.0)); //
+		figures.add(new Plane(new Point(1, 0, 0), new Direction(-1, 0, 0), Color.RED, 0, 0.5, 0, 0.0)); // izq
+		figures.add(new Plane(new Point(-1, 0, 0), new Direction(1, 0, 0), Color.GREEN, 80, 0.5, 0, 0.0));// drcha
 		// figures.add(new Sphere(new Point(-20, -20, 50), 5, Color.BLUE, 0.8));
-		figures.add(new Sphere(new Point(-70, -20, 50), 5, Color.BLUE, 0.0, 1));
-		figures.add(new Sphere(new Point(-40, -20, 50), 5, Color.BLUE, 0.0, 1));
-		//figures.add(new Sphere(new Point(-40, -10, 50), 5, Color.RED, 1, 0));
+		figures.add(new Sphere(new Point(-70, -20, 50), 5, Color.BLUE, 0.0, 1, 0.0));
+		figures.add(new Sphere(new Point(-40, -20, 50), 5, Color.BLUE, 0.0, 1, 0.0));
+		// figures.add(new Sphere(new Point(-40, -10, 50), 5, Color.RED, 1, 0));
 		// figures.add(new Triangle(new Point(-50, -10, 50),new Point(-30, -10, 40),new
 		// Point(-30, -10, 50),new Direction(1, 1, 1), Color.ORANGE,0.5));
 		figures.add(new Triangle(new Point(-5, -20, 50), new Point(-10, -20, 50), new Point(-7.5, -20, 60),
-				new Direction(0, 1, 0), Color.ORANGE, 0.5, 0.0, false));
+				new Direction(0, 1, 0), Color.ORANGE, 0.5, 0.0, 0.0, false));
 		figures.add(new Triangle(new Point(-35, 39.99, 45), new Point(-45, 39.99, 55), new Point(-45, 39.99, 45),
-				new Direction(0, 1, 0), Color.WHITE, 0.5, 0.0, true));
+				new Direction(0, 1, 0), Color.WHITE, 0.5, 0.0, 0.0, true));
 		figures.add(new Triangle(new Point(-35, 39.99, 55), new Point(-35, 39.99, 45), new Point(-45, 39.99, 55),
-				new Direction(0, 1, 0), Color.WHITE, 0.5, 0.0, true));
+				new Direction(0, 1, 0), Color.WHITE, 0.5, 0.0, 0.0, true));
 		figures.add(new Triangle(new Point(-20, -20, 50), new Point(-30, -20, 50), new Point(-30, -20, 60),
-				new Direction(0, 1, 0), Color.ORANGE, 0.5, 0.0, false));
+				new Direction(0, 1, 0), Color.ORANGE, 0.5, 0.0, 0.0, false));
 		figures.add(new Triangle(new Point(-20, -20, 50), new Point(-20, -20, 60), new Point(-30, -20, 60),
-				new Direction(0, 1, 0), Color.ORANGE, 0.5, 0.0, false));
+				new Direction(0, 1, 0), Color.ORANGE, 0.5, 0.0, 0.0, false));
 		// figures.add(new Triangle(new Point(-60, 10, 40),new Point(-60, 10, 50),new
 		// Point(-50, 10, 40),new Direction(1, 1, 1), Color.ORANGE,0.5));
 		// figures.add(new Sphere(new Point(0, -20, 50), 5, Color.BLUE, 0.8));
@@ -110,20 +109,23 @@ public class PathTracer {
 	}
 
 	public static Color brighter(Color color, double percent) {
-		int red = (int)(color.getRed() + (color.getRed()* percent));
-		if(red>255) red = 255;
-		int green = (int) (color.getGreen() +( color.getGreen()* percent));
-		if(green>255) green = 255;
-		int blue = (int) (color.getBlue() + (color.getBlue()* percent));
-		if(blue>255) blue = 255;
-		
-      return new Color(red, green, blue);
+		int red = (int) (color.getRed() + (color.getRed() * percent));
+		if (red > 255)
+			red = 255;
+		int green = (int) (color.getGreen() + (color.getGreen() * percent));
+		if (green > 255)
+			green = 255;
+		int blue = (int) (color.getBlue() + (color.getBlue() * percent));
+		if (blue > 255)
+			blue = 255;
+
+		return new Color(red, green, blue);
 	}
-	
+
 	private static void percent(int py, int y) {
-		if((py * 100) / y > percent) {
-			percent = (py * 100) / y ;
-			System.out.println((percent+1)+"%");
+		if ((py * 100) / y > percent) {
+			percent = (py * 100) / y;
+			System.out.println((percent + 1) + "%");
 		}
 	}
 
@@ -135,11 +137,11 @@ public class PathTracer {
 		int bA = 0;
 		Point pA;
 //		Point light = new Point(-35, 39.98, 45);
-		Triangle lightT1=new Triangle(new Point(-35, 39.98, 45), new Point(-45, 39.98, 55), new Point(-45, 39.98, 45),
-				new Direction(0, 1, 0), Color.WHITE, 0.5, 0.2, true);
+		Triangle lightT1 = new Triangle(new Point(-35, 39.98, 45), new Point(-45, 39.98, 55), new Point(-45, 39.98, 45),
+				new Direction(0, 1, 0), Color.WHITE, 0.5, 0.2, 0.0, true);
 		lightT1.setIs_light(true);
-		Triangle lightT2=new Triangle(new Point(-35, 39.98, 55), new Point(-35, 39.98, 45), new Point(-45, 39.98, 55),
-				new Direction(0, 1, 0), Color.WHITE, 0.5, 0.2, true);
+		Triangle lightT2 = new Triangle(new Point(-35, 39.98, 55), new Point(-35, 39.98, 45), new Point(-45, 39.98, 55),
+				new Direction(0, 1, 0), Color.WHITE, 0.5, 0.2, 0.0, true);
 		lightT2.setIs_light(true);
 		Point light = lightT1.getRnd();
 		Point light2 = lightT2.getRnd();
@@ -173,14 +175,14 @@ public class PathTracer {
 			// Direct light Blanco si choca en luz
 			Color ld;
 			if (!f.is_light)
-				ld = luzDirecta(intersection, light, figures, f,80);
+				ld = luzDirecta(intersection, light, figures, f, 80);
 			else
 				ld = Color.WHITE;
-			
+
 			int mC = RussianRoulette.calculate(f);
 			// MonteCarlo
-			Color lI = new Color(0,0,0);
-			
+			Color lI = new Color(0, 0, 0);
+
 			/*
 			 * if (mC == 1) { double i = Operator.dotProduct(rayPath.getD(),
 			 * f.getNormal(intersection)); rayPath.setD(Operator.subD(rayPath.getD(),
@@ -189,74 +191,64 @@ public class PathTracer {
 			 */
 			Ray tt = rayPath;
 			if (mC == 2) {
-				lI = luzIndirectaEspecular(intersection, light, figures, f, tt,0);
-			}
-			else if(mC == 1) {
-				lI = luzIndirectaDifusa(intersection, light, figures, f, tt,0);
+				lI = luzIndirectaEspecular(intersection, light, figures, f, tt, 0);
+			} else if (mC == 1) {
+				lI = luzIndirectaDifusa(intersection, light, figures, f, tt, 0);
 			}
 			// end ray
 
-				rA =rA + (ld.getRed() + lI.getRed())/2;
-				bA =bA + (ld.getBlue() + lI.getBlue())/2;
-				gA = gA + (ld.getGreen() + lI.getGreen())/2;
-				
-				//PARA PROBAR INDIRECTA SOLO DESCOMENTAR
+			rA = rA + (ld.getRed() + lI.getRed()) / 2;
+			bA = bA + (ld.getBlue() + lI.getBlue()) / 2;
+			gA = gA + (ld.getGreen() + lI.getGreen()) / 2;
+
+			// PARA PROBAR INDIRECTA SOLO DESCOMENTAR
 //			rA =rA + lI.getRed();
 //			bA =bA +  lI.getBlue();
 //			gA = gA +  lI.getGreen();
 		}
-		
+
 		rA = rA / (antialiasing);
 		bA = bA / (antialiasing);
 		gA = gA / (antialiasing);
-		if (rA > 255) {
-			rA = 255;
-		}
-		if (bA > 255) {
-			bA = 255;
-		}
-		if (gA > 255) {
-			gA = 255;
-		}
-		
-		Color endColor = new Color(rA, gA, bA);
-		return ( endColor);
+
+		Color endColor = limitColor(rA, gA, bA);
+		return (endColor);
 
 	}
 
-	private static void createCoordinateSystem(Direction n1){
-		float x,y,z;
-		if(Math.abs(n1.getX()) > Math.abs(n1.getY())){
+	private static void createCoordinateSystem(Direction n1) {
+		float x, y, z;
+		if (Math.abs(n1.getX()) > Math.abs(n1.getY())) {
 			x = (float) (n1.getZ() / Math.sqrt(n1.getX() * n1.getX() + n1.getZ() * n1.getZ()));
 			y = 0;
 			z = (float) (-n1.getX() / Math.sqrt(n1.getX() * n1.getX() + n1.getZ() * n1.getZ()));
-		}
-		else {
+		} else {
 			x = 0;
 			y = (float) (-n1.getZ() / Math.sqrt(n1.getY() * n1.getY() + n1.getZ() * n1.getZ()));
 			z = (float) (n1.getY() / Math.sqrt(n1.getY() * n1.getY() + n1.getZ() * n1.getZ()));
 		}
-		Nt = new Direction(x,y,z);
+		Nt = new Direction(x, y, z);
 		Nb = Operator.crossProduct(n1, Nt);
 	}
-	private static Color luzIndirectaDifusa(Point intersect, Point light, List<Figure> figures, 
-			Figure fi, Ray ray,  int bounds) {
+
+	private static Color luzIndirectaDifusa(Point intersect, Point light, List<Figure> figures, Figure fi, Ray ray,
+			int bounds) {
 		// new ray
 		/******************************/
 		double cosTheta = Math.random();
-	    double sinTheta = Math.sqrt(1 - cosTheta * cosTheta);
+		double sinTheta = Math.sqrt(1 - cosTheta * cosTheta);
 		Direction rayDirection = getSample(cosTheta);
 		createCoordinateSystem(fi.getNormal(intersect));
 
-		Direction rayDirectionCast = new Direction(rayDirection.getX() * Nb.getX() 
-				+ rayDirection.getY() * fi.getNormal(intersect).getX() 
-				+ rayDirection.getZ() * Nt.getX(),
-				rayDirection.getX() * Nb.getY() + rayDirection.getY() * fi.getNormal(intersect).getY() 
-				+ rayDirection.getZ() * Nt.getY(),
-				rayDirection.getX() * Nb.getZ() + rayDirection.getY() * fi.getNormal(intersect).getZ() 
-				+ rayDirection.getZ() * Nt.getZ());
+		Direction rayDirectionCast = new Direction(
+				rayDirection.getX() * Nb.getX() + rayDirection.getY() * fi.getNormal(intersect).getX()
+						+ rayDirection.getZ() * Nt.getX(),
+				rayDirection.getX() * Nb.getY() + rayDirection.getY() * fi.getNormal(intersect).getY()
+						+ rayDirection.getZ() * Nt.getY(),
+				rayDirection.getX() * Nb.getZ() + rayDirection.getY() * fi.getNormal(intersect).getZ()
+						+ rayDirection.getZ() * Nt.getZ());
 		Ray r = new Ray(Operator.addD(intersect, rayDirectionCast), rayDirectionCast);
-		ray=r;
+		ray = r;
 		/******************************/
 		Figure f = null;
 		double s = 9999999;
@@ -268,51 +260,59 @@ public class PathTracer {
 			if (s > t && t > 0) {
 				s = t;
 				f = sf;
-				//direct light
+				// direct light
 				intersection = Operator.addD(intersect, ray.getD().scale(t - 0.000001));
 				// Light of direct Light
 			}
 		}
-		Color lD = luzDirecta(intersection, light, figures, f,80);
+		Color lD = luzDirecta(intersection, light, figures, f, 80);
 		Color lD2 = new Color(0);
-		if(bounds < MAX_REBOTES && !f.is_light){
+		if (bounds < MAX_REBOTES && !f.is_light) {
 			int russianRoulette = RussianRoulette.calculate(f);
-			if ( russianRoulette == 1 && !f.is_light) {
-				lD2 = luzIndirectaDifusa(intersection, light, figures, f, ray,bounds + 1);
+			if (russianRoulette == 1 && !f.is_light) {
+				lD2 = luzIndirectaDifusa(intersection, light, figures, f, ray, bounds + 1);
 ////				lD = new Color((int)255/(bounds+1), 0 ,(int)255/(bounds+1));
 
-			}
-			else if (russianRoulette == 2  && !f.is_light) {
-				lD2= luzIndirectaEspecular(intersection, light, figures, f, ray,bounds + 1);	
+			} else if (russianRoulette == 2 && !f.is_light) {
+				lD2 = luzIndirectaEspecular(intersection, light, figures, f, ray, bounds + 1);
 //				lD = new Color(((lD2.getRed() + lD.getRed()) / 2), ((lD2.getGreen() + lD.getGreen()) / 2),
 //				((lD2.getBlue() + lD.getBlue()) / 2));
 			}
-			double rr =lD2.getRed()*f.getKd()/(bounds+1);//(2*sinTheta*cosTheta);//Operator.subP(intersection, intersect).module();//
-			double bb =lD2.getBlue()*f.getKd()/(bounds+1);//(2*sinTheta*cosTheta);//Operator.subP(intersection, intersect).module();//(bounds+1);
-			double gg = lD2.getGreen()*f.getKd()/(bounds+1);//(2*sinTheta*cosTheta);//Operator.subP(intersection, intersect).module();//(bounds+1);
-			
-			double rFinal = (lD.getRed()+rr)/2;
-			double bFinal = (lD.getBlue()+bb)/2;
-			double gFinal = (lD.getGreen()+gg)/2;
-			
-			lD= limitColor((int)rFinal,(int)gFinal,(int)bFinal);
+			double rr = lD2.getRed() * f.getKd() / (bounds + 1);// (2*sinTheta*cosTheta);//Operator.subP(intersection,
+																// intersect).module();//
+			double bb = lD2.getBlue() * f.getKd() / (bounds + 1);// (2*sinTheta*cosTheta);//Operator.subP(intersection,
+																	// intersect).module();//(bounds+1);
+			double gg = lD2.getGreen() * f.getKd() / (bounds + 1);// (2*sinTheta*cosTheta);//Operator.subP(intersection,
+																	// intersect).module();//(bounds+1);
+
+			double rFinal = (lD.getRed() + rr) / 2;
+			double bFinal = (lD.getBlue() + bb) / 2;
+			double gFinal = (lD.getGreen() + gg) / 2;
+
+			lD = limitColor((int) rFinal, (int) gFinal, (int) bFinal);
 		}
 		return lD;
 	}
 
 	public static Color limitColor(int r, int g, int b) {
-		if(r>255) r=255;
-		if (r<0) r=0;
-		if(g>255) g=255;
-		if (g<0) g=0;
-		if(b>255) b=255;
-		if (b<0) b=0;
-		return new Color(r,g,b);
+		if (r > 255)
+			r = 255;
+		if (r < 0)
+			r = 0;
+		if (g > 255)
+			g = 255;
+		if (g < 0)
+			g = 0;
+		if (b > 255)
+			b = 255;
+		if (b < 0)
+			b = 0;
+		return new Color(r, g, b);
 	}
-	
-	private static Color luzIndirectaEspecular(Point intersect, Point light, List<Figure> figures, 
-			Figure fi, Ray ray, int bounds) {
-		// new specular 
+
+	private static Color luzIndirectaEspecular(Point intersect, Point light, List<Figure> figures, Figure fi, Ray ray,
+			int bounds) {
+		// new specular
 		/******************************/
 		double i = Operator.dotProduct(ray.getD(), fi.getNormal(intersect));
 		ray.setD(Operator.subD(ray.getD(), fi.getNormal(intersect).scale(2 * i)));
@@ -332,16 +332,16 @@ public class PathTracer {
 				// // Light of direct Light
 			}
 		}
-		Color lE = luzDirecta(intersection, light, figures, f,80);
+		Color lE = luzDirecta(intersection, light, figures, f, 80);
 		Color lE2 = new Color(0);
 		// while(!f.is_light) {
 		// RussianRoullete
-		if(bounds < MAX_REBOTES && !f.is_light){
+		if (bounds < MAX_REBOTES && !f.is_light) {
 			int russianRoulette = RussianRoulette.calculate(f);
-			if ( russianRoulette == 1) {
-				lE2 = luzIndirectaDifusa(intersection, light, figures, f, ray,bounds + 1);
+			if (russianRoulette == 1) {
+				lE2 = luzIndirectaDifusa(intersection, light, figures, f, ray, bounds + 1);
 			}
-			if ( russianRoulette == 2) {
+			if (russianRoulette == 2) {
 				lE2 = luzIndirectaEspecular(intersection, light, figures, f, ray, bounds + 1);
 			}
 			lE = new Color(((lE2.getRed() + lE.getRed()) / 2), ((lE2.getGreen() + lE.getGreen()) / 2),
@@ -354,33 +354,23 @@ public class PathTracer {
 		// direction de intersection a ligth
 		Direction cx = Operator.subP(light, intersect);
 		cx.normalize(); // normalizada
-		Ray r = new Ray(intersect, cx);
-		if (block(f, r, light)) {
-			return Color.BLACK;
-		} else {
-			int rr = (int) colour(255, light, intersect, fi, fi.getColor().getRed(),  potencia);
-			int gg = (int) colour(255, light, intersect, fi, fi.getColor().getGreen(),  potencia);
-			int bb = (int) colour(255, light, intersect, fi, fi.getColor().getBlue(),  potencia);
-			if (rr > 255) {
-				rr = 255;
+		if(fi.getKr()>0.0) {
+			//calcular nueva theta
+			//intersectar el rayo
+			//calcular luz directa
+			return null;
+		}
+		else {
+			Ray r = new Ray(intersect, cx);
+			if (block(f, r, light)) {
+				return Color.BLACK;
+			} else {
+				int rr = (int) colour(255, light, intersect, fi, fi.getColor().getRed(),  potencia);
+				int gg = (int) colour(255, light, intersect, fi, fi.getColor().getGreen(),  potencia);
+				int bb = (int) colour(255, light, intersect, fi, fi.getColor().getBlue(),  potencia);
+				Color c = limitColor(rr, gg, bb);
+				return c;
 			}
-			if (rr < 0) {
-				rr = 0;
-			}
-			if (bb > 255) {
-				bb = 255;
-			}
-			if (bb < 0) {
-				bb = 0;
-			}
-			if (gg > 255) {
-				gg = 255;
-			}
-			if (gg < 0) {
-				gg = 0;
-			}
-			Color c = new Color(rr, gg, bb);
-			return c;
 		}
 	}
 
@@ -424,14 +414,14 @@ public class PathTracer {
 	public static double difuseComponent(double kd, Direction normal, Direction toLight) {
 		return kd * Operator.dotProduct(normal, toLight);
 	}
-	
+
 	public static double monteCarlo() {
 		return Math.toRadians(Math.random() * 180);
 	}
-	
-	public static Direction getSample(double cosTheta){
-	    double sinTheta = Math.sqrt(1 - cosTheta * cosTheta);
-	    double phi = Math.PI * 2 * Math.random();
-	    return new Direction(sinTheta * Math.cos(phi), cosTheta, sinTheta * Math.sin(phi));
+
+	public static Direction getSample(double cosTheta) {
+		double sinTheta = Math.sqrt(1 - cosTheta * cosTheta);
+		double phi = Math.PI * 2 * Math.random();
+		return new Direction(sinTheta * Math.cos(phi), cosTheta, sinTheta * Math.sin(phi));
 	}
 }
